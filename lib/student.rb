43378cc713f759306da948f8f3510eb27e978476
Mike Merin <mikemerinweather@gmail.com>
2017-04-12 16:46:51 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -54,7 +54,7 @@ class Student
   def update
     sql = <<-SQL
       UPDATE students
-      SET (name, grade) = (?, ?)
+      SET name = ?, grade = ?
       WHERE id = ?
     SQL
     DB[:conn].execute(sql, self.name, self.grade, self.id)
